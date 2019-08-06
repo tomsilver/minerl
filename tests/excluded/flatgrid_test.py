c27aa9e4646cf6e8482040383a4805fcb374041e
Tom Silver <tom@Toms-MacBook-Pro-3.local>
2019-08-05 20:18:24 -0400

    flat grid environment

---
+++ tests/excluded/flatgrid_test.py
@@ -0,0 +1,76 @@
+# Simple env test.
+import logging
+
+import matplotlib.pyplot as plt
+
+import gym
+import minerl
+
+import numpy as np
+
+import coloredlogs
+coloredlogs.install(logging.INFO)
+
+
+
+def main():
+    """
+    Tests running a simple environment.
+    """
+    all_positions = []
+
+    env = gym.make('MineRLFlatGrid-v0')
+    env.seed(420)
+    obs = env.reset()
+    position = (obs['XPos'], obs['YPos'], obs['ZPos'])
+    all_positions.append(position)
+    done = False
+    while not done:
+        action = env.action_space.noop()
+        action['forward'] = 1
+        obs, reward, done, info = env.step(action)
+        position = (obs['XPos'], obs['YPos'], obs['ZPos'])
+        all_positions.append(position)
+
+    ts = np.arange(len(all_positions))
+    xs, ys, zs = np.transpose(all_positions)
+    dxs = np.subtract(xs[1:], xs[:-1])
+    dys = np.subtract(ys[1:], ys[:-1])
+    dzs = np.subtract(zs[1:], zs[:-1])
+    ddxs = np.subtract(dxs[1:], dxs[:-1])
+    ddys = np.subtract(dys[1:], dys[:-1])
+    ddzs = np.subtract(dzs[1:], dzs[:-1])
+
+    fig, axes = plt.subplots(3, 3, sharex=True)
+    axes[0][0].plot(ts, xs)
+    axes[0][1].plot(ts, ys)
+    axes[0][2].plot(ts, zs)
+
+    axes[0][0].set_title("XPos")
+    axes[0][1].set_title("YPos")
+    axes[0][2].set_title("ZPos")
+
+    axes[1][0].plot(ts[1:], dxs)
+    axes[1][1].plot(ts[1:], dys)
+    axes[1][2].plot(ts[1:], dzs)
+
+    axes[1][0].set_title("Delta XPos")
+    axes[1][1].set_title("Delta YPos")
+    axes[1][2].set_title("Delta ZPos")
+
+    axes[2][0].plot(ts[2:], ddxs)
+    axes[2][1].plot(ts[2:], ddys)
+    axes[2][2].plot(ts[2:], ddzs)
+
+    axes[2][0].set_title("Delta Delta XPos")
+    axes[2][1].set_title("Delta Delta YPos")
+    axes[2][2].set_title("Delta Delta ZPos")
+
+    plt.tight_layout()
+    plt.show()
+
+
+    print("Demo complete.")
+
+if __name__ == "__main__":
+    main()
